1f6d3283fb10fd28b3d2b81dbdfdb5eadab1146e
Matthew Kimball <mekimba6@gmail.com>
2022-01-04 17:16:07 -0700

    adds bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :teams, only: %i[index show] do
     resources :players, only: %i[index]
